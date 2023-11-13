47a035be50d44e6fe3cd7baa1cbade50de00703e
satte1 <f.satti@maharah.com>
2023-11-13 13:21:59 +0300

    Add workflow and themes

--- TurstedB.DataAccess/Data/ApplicationDbContext.cs
+++ TurstedB.DataAccess/Data/ApplicationDbContext.cs
@@ -15,7 +15,6 @@ namespace TrustedB.DataAccess.Data
         }
 
        
-
         public DbSet<Topics> Topics { get; set; }
         
 
--- TurstedBWeb/wwwroot/js/site.js
+++ TurstedBWeb/wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+const stepButtons = document.querySelectorAll('.step-button');
+const progress = document.querySelector('#progress');
+var form = document.getElementById("form-id");
+var step = 2;
 
-// Write your JavaScript code.
+Array.from(stepButtons).forEach((button, index) => {
+    if (index < step) { button.classList.add('done') }
+    progress.setAttribute('value', step * 100 / (stepButtons.length - 1));
+});
+
+Array.from(stepButtons).forEach((button, index) => {
+    button.addEventListener('click', () => {
+        progress.setAttribute('value', index * 100 / (stepButtons.length - 1));//there are 3 buttons. 2 spaces.
+        //stepButtons.classList.add('done');
+        stepButtons.forEach((item, secindex) => {
+
+            if (index >= secindex) {
+                item.classList.add('done');
+                //document.getElementById("your-id").addEventListener("click", function () {
+                //    form.submit();
+                //});
+
+            }
+            if (index < secindex) {
+                item.classList.remove('done');
+                //document.getElementById("your-id").addEventListener("click", function () {
+                //    form.submit();
+                //});
+            }
+        })
+    })
+});
