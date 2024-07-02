f0e848d28efc5d85c940ea7bcc75f29acc17684e
Fakhreldeen Satti <Fakhreldeen.satti@gmail.com>
2024-07-02 22:43:53 +0300

    some update

--- TurstedBWeb/Areas/Customer/Controllers/HomeController.cs
+++ TurstedBWeb/Areas/Customer/Controllers/HomeController.cs
@@ -103,7 +103,7 @@ namespace TrustedBWeb.Areas.Customer.Controllers
             byte[] bytes = System.IO.File.ReadAllBytes(filePath);
 
             //Send the File to Download.
-            return File(bytes, "application/octet-stream", "Audio.mp3");
+            return File(bytes, "application/octet-stream", "Image.png");
         }
 
         //______________________________Audio____________________________________
@@ -133,21 +133,18 @@ namespace TrustedBWeb.Areas.Customer.Controllers
 
         //______________________________Audio____________________________________
 
-        public IActionResult AllImages(int pg = 1)
+        public IActionResult AllImages(int? SubCategoryID, int pg = 1)
         {
-
-         
 
             const int pageSize = 4;
             if (pg < 1) { pg = 1; }
 
-            int recsCount = _unitOfWork.Topics.GetAll(filter: o => (o.CategoryID == 3)).Count();
+            int recsCount = _unitOfWork.Topics.GetAll(filter: o => (o.SubCategoryID == SubCategoryID)).Count();
             var pager = new Pager(recsCount, pg, pageSize);
             int recSkip = (pg - 1) * pageSize;
 
             //GetAllPagination
-            var ImageList = _unitOfWork.Topics.GetAllPaginationAudio(recSkip, pager.PageSize, filter: o => (o.CategoryID == 3)).ToList();
-          
+            var ImageList = _unitOfWork.Topics.GetAllPaginationAudio(recSkip, pager.PageSize, filter: o => (o.SubCategoryID == SubCategoryID)).ToList();
 
             //var TopicList = _unitOfWork.Topics.GetAll(filter: o => (o.CategoryID == 4)).ToList();
             this.ViewBag.Pager = pager;
