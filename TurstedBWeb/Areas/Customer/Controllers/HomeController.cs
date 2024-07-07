9b209b818ebfc4ed47e46acbd753f9632f6f8d02
Fakhreldeen Satti <Fakhreldeen.satti@gmail.com>
2024-07-07 17:10:32 +0300

    image size

--- TurstedBWeb/Areas/Customer/Controllers/HomeController.cs
+++ TurstedBWeb/Areas/Customer/Controllers/HomeController.cs
@@ -164,12 +164,12 @@ namespace TrustedBWeb.Areas.Customer.Controllers
             const int pageSize = 3;
             if (pg < 1) { pg = 1; }
 
-            int recsCount = _unitOfWork.Topics.GetAll(filter: o => (o.CategoryID == 3)).Count();
+            int recsCount = _unitOfWork.Topics.GetAll(filter: o => (o.CategoryID == SubCategoryID)).Count();
             var pager = new Pager((int)SubCategoryID, recsCount, pg, pageSize);
             int recSkip = (pg - 1) * pageSize;
 
             //GetAllPagination
-            var ImageList = _unitOfWork.Topics.GetAllPaginationAudio(recSkip, pager.PageSize, filter: o => (o.SubCategoryID == SubCategoryID)&&(o.CategoryID==3)).ToList();
+            var ImageList = _unitOfWork.Topics.GetAllPaginationAudio(recSkip, pager.PageSize, filter: o => (o.SubCategoryID == SubCategoryID)).ToList();
 
             //var TopicList = _unitOfWork.Topics.GetAll(filter: o => (o.CategoryID == 4)).ToList();
             this.ViewBag.Pager = pager;
