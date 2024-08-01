46d14ac00cd54fedec3465c25fd1d139c6ed8a61
satte1 <f.satti@maharah.com>
2024-08-01 16:48:24 +0300

    Translate

--- TurstedBWeb/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ TurstedBWeb/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -62,7 +62,7 @@ namespace TrustedBWeb.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = await _userManager.FindByEmailAsync(Input.Email);
-                if (user == null )
+                if (user == null)
                 {
                     // Don't reveal that the user does not exist or is not confirmed
                     return RedirectToPage("./ForgotPasswordConfirmation");
